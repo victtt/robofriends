808915892a30eae3df935b2f03965ff38f568c6c
Vincent <Vincent@VincentdeMacBook-Air.local>
2020-04-28 19:52:33 +0800

    Updates

---
+++ precache-manifest.6d27630e68f8730790ccef08e24cc569.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92e8e8e2d8d523fedbe2c140af5f4d15",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d1d700ca0dae124b0e5e",
+    "url": "/my-app/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "3bcd2567861dde85aa8c",
+    "url": "/my-app/static/css/main.a4c700fa.chunk.css"
+  },
+  {
+    "revision": "d1d700ca0dae124b0e5e",
+    "url": "/my-app/static/js/2.2b73e027.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.2b73e027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bcd2567861dde85aa8c",
+    "url": "/my-app/static/js/main.bed0c5fd.chunk.js"
+  },
+  {
+    "revision": "ceff16123aa9ca3ca553",
+    "url": "/my-app/static/js/runtime-main.19c4b6ef.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/my-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
